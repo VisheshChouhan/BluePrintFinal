f1d8ff22e34e2b0fc83993327820f7cdc3982896
jaindivyanshu1 <jaindivyanshu.personal@gmail.com>
2024-07-05 21:44:40 +0530

    "something done"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:blue_print/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:blue_print_final/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
